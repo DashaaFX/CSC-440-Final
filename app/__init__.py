4fc569fd59864b2d44fc852d710562cbb7b797dd
natalietthomas <121183224+natalietthomas@users.noreply.github.com>
2025-12-02 18:05:28 -0500

    Frontend updates
    Co-authored-by: Raunak Abichandani <raunakabichandani@mail.adelphi.edu>

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,30 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
 
-    # home route
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    @app.route('/register')
+    def register():
+        return render_template('register.html')
+
+    @app.route('/dashboard')
+    def dashboard():
+        return render_template('dashboard.html')
+    
+    @app.route('/new_ticket')
+    def new_ticket():
+        return render_template('new_ticket.html')
+
+    @app.route('/tickets')
+    def tickets():
+        return render_template('tickets.html')
+
+
 
     return app
